3c38c14905a560159baf73e0712a30039d255284
gdsanders <gd@digitalequity.com>
2015-08-20 21:41:01 -0500

    convertButtonPressed fun

--- ViewController.swift
+++ ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalWithTipLabel: UILabel!
     
+    var tipValue: Double? = 15
+    
     
 
     override func viewDidLoad() {
@@ -29,7 +31,20 @@ class ViewController: UIViewController {
     
     @IBAction func convertButtonPressed(sender: UIButton) {
         billTextField.resignFirstResponder()
-        print(billTextField.text!)
+       
+        if billTextField.text! != "" && billTextField.text != nil {
+            if tipValue! != 0 && tipValue != nil {
+                tipLabel.text = "\(tipValue!)"
+                
+                let totalWithTip = tipValue! + Double(billTextField.text!)!
+                totalWithTipLabel.text = "\(totalWithTip)"
+            }
+        else {
+                tipLabel.text = "No tip"
+                totalWithTipLabel.text = "\(billTextField.text!)"
+            }
+            
+        }
         
     }
     
